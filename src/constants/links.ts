3166c0e31fc53598d3545b839cd2f805c20dce01
JomarMonrealUPLB <jpmonreal@up.edu.ph>
2024-06-20 17:29:50 +0800

    feat: added links

---
+++ src/constants/links.ts
@@ -0,0 +1,26 @@
+export const links = [
+    {
+        "path": "/",
+        "label": "Home",
+        "visibility": ["all"],
+        "visibleInNavBar": true
+    },
+    {
+        "path": "*",
+        "label": "Page Doesn't Exist",
+        "visibility": ["all"],
+        "visibleInNavBar": false
+    },
+    {
+        "path": "/about",
+        "label": "About",
+        "visibility": ["all"],
+        "visibleInNavBar": true
+    },
+    {
+        "path": "/Tutorials",
+        "label": "Tutorials",
+        "visibility": ["all"],
+        "visibleInNavBar": true
+    }
+]
