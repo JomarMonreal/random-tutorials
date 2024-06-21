1ad0c2312686551ebf7fdc0e181281bdff6e4739
JomarMonrealUPLB <jpmonreal@up.edu.ph>
2024-06-21 22:26:37 +0800

    refactor: moved main background to new file

---
+++ src/components/MainBackground.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import background from "../../public/images/background.webp";
+
+export function MainBackground() {
+  return <div className="main-background">
+    <Image
+      src={background}
+      alt={"Background"}
+      fill
+      style={{ objectFit: "cover" }}
+      quality={100}
+      priority />
+  </div>;
+}
