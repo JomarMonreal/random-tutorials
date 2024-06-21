c9f9a79598f0d16559cec8718949e9841fc0ab0d
JomarMonrealUPLB <jpmonreal@up.edu.ph>
2024-06-21 13:47:16 +0800

    feat: added Partly Orange Text

---
+++ src/components/PartlyOrangeText.tsx
@@ -0,0 +1,43 @@
+import { getRandomInt } from "@/utils/randomizer"
+import { Orange } from "./Colors"
+import { useEffect, useState } from "react"
+
+const PartlyOrangeText = ({orangeLettersCount, text, delay}:{orangeLettersCount: number, text: string, delay: number}) => {
+
+    const [result, setResult] = useState<React.JSX.Element[]>()
+
+    useEffect(() => {
+        changePartsToOrange()
+        const changePartsToOrangeByInterval = setInterval(changePartsToOrange,delay)
+
+        return ()=> clearInterval(changePartsToOrangeByInterval)
+
+    }, []);
+
+    const changePartsToOrange = () =>{
+        const orangeLetterIndices = new Set()
+    
+        while (orangeLetterIndices.size <  orangeLettersCount) {
+            const randomIndex = getRandomInt(text.length)
+            orangeLetterIndices.add(randomIndex) 
+        }
+    
+        const textArray = text.split("")
+        setResult(
+            textArray.map((char,index)=>{
+                if(orangeLetterIndices.has(index)){
+                    return (
+                        <Orange key={index}>{char}</Orange>
+                    )
+                }
+                return <span key={index}>{char}</span>
+            })
+        ) 
+    }
+
+    return (
+        <>{result}</>
+    )
+}
+
+export default PartlyOrangeText
