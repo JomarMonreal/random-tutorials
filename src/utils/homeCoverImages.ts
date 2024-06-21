5b87cceb866722cede398e55c7c2f7056d01b65c
JomarMonrealUPLB <jpmonreal@up.edu.ph>
2024-06-21 13:48:04 +0800

    chore: added more home cover images

--- src/utils/homeCoverImages.ts
+++ src/utils/homeCoverImages.ts
@@ -1,3 +1,5 @@
 export const homeCoverImages = [
-    "https://startupstockphotos.com/wp-content/uploads/2021/12/ssp014.jpg"
+    "https://startupstockphotos.com/wp-content/uploads/2021/12/ssp014.jpg",
+    "https://i.ytimg.com/vi/xx8NZa4ptJA/maxresdefault.jpg",
+    "https://images.squarespace-cdn.com/content/v1/5eaa851bc7b0ae39a020ea32/2d9a8b2c-e0ba-4143-bbfa-255dd67d4353/Backflip+Tutorial+%281%29.jpg?format=2500w"
 ]
