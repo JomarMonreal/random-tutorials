53469b527a5c11e1d75e82284b3dbf79b880d491
JomarMonrealUPLB <jpmonreal@up.edu.ph>
2024-06-22 19:45:35 +0800

    chore: added project cards

--- src/app/tutorials/page.tsx
+++ src/app/tutorials/page.tsx
@@ -1,10 +1,58 @@
+import { theme } from "@/themes/themes"
+import { ThemeProvider } from "@mui/material"
+import { ProjectCard } from "../../components/ProjectCard"
 
 const Tutorials = () => {
     return (
-        <main>
-            <h1>Tutorials</h1>
-        </main>
+        <ThemeProvider theme={theme}>
+            <main className="flex justify-center flex-wrap">
+                <ProjectCard 
+                    title={"How to Open a Door?"} 
+                    username={"DummyUser"} 
+                    date={"March 19, 2024"} 
+                    description={"This is a comprehensive step-by-step tutorial on how to open a door."} 
+                    imageUrl={"https://i.ytimg.com/vi/xx8NZa4ptJA/maxresdefault.jpg"}
+                />
+                <ProjectCard 
+                    title={"How to Open a Door?"} 
+                    username={"DummyUser"} 
+                    date={"March 19, 2024"} 
+                    description={"This is a comprehensive step-by-step tutorial on how to open a door."} 
+                    imageUrl={"https://i.ytimg.com/vi/xx8NZa4ptJA/maxresdefault.jpg"}
+                />
+                <ProjectCard 
+                    title={"How to Open a Door?"} 
+                    username={"DummyUser"} 
+                    date={"March 19, 2024"} 
+                    description={"This is a comprehensive step-by-step tutorial on how to open a door."} 
+                    imageUrl={"https://i.ytimg.com/vi/xx8NZa4ptJA/maxresdefault.jpg"}
+                />
+                <ProjectCard 
+                    title={"How to Open a Door?"} 
+                    username={"DummyUser"} 
+                    date={"March 19, 2024"} 
+                    description={"This is a comprehensive step-by-step tutorial on how to open a door."} 
+                    imageUrl={"https://i.ytimg.com/vi/xx8NZa4ptJA/maxresdefault.jpg"}
+                />
+                <ProjectCard 
+                    title={"How to Open a Door?"} 
+                    username={"DummyUser"} 
+                    date={"March 19, 2024"} 
+                    description={"This is a comprehensive step-by-step tutorial on how to open a door."} 
+                    imageUrl={"https://i.ytimg.com/vi/xx8NZa4ptJA/maxresdefault.jpg"}
+                />
+                <ProjectCard 
+                    title={"How to Open a Door?"} 
+                    username={"DummyUser"} 
+                    date={"March 19, 2024"} 
+                    description={"This is a comprehensive step-by-step tutorial on how to open a door."} 
+                    imageUrl={"https://i.ytimg.com/vi/xx8NZa4ptJA/maxresdefault.jpg"}
+                />
+            </main>
+        </ThemeProvider>
     )
 }
 
 export default Tutorials
+
+
