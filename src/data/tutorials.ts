ff66c1ccc6bf23488a3c19fe7da14a82b7359647
JomarMonreal <jpmonreal2035@gmail.com>
2024-06-22 22:05:58 +0800

    feat: added tutorials dummy data

---
+++ src/data/tutorials.ts
@@ -0,0 +1,25 @@
+export const tutorials = [
+    {
+        title: "How to Open a Door?",
+        username: "DummyUser",
+        date: "2024-06-22T12:24:34.544Z",
+        description: "This is a comprehensive step-by-step tutorial on how to open a door.",
+        coverImageUrl: "https://i.ytimg.com/vi/xx8NZa4ptJA/maxresdefault.jpg",
+
+    },
+    {
+        title: "How to Backflip?",
+        username: "DummyUser",
+        date: "2024-06-22T12:24:34.544Z",
+        description: "This is a comprehensive step-by-step tutorial on how to backflip.",
+        coverImageUrl: "https://images.squarespace-cdn.com/content/v1/5eaa851bc7b0ae39a020ea32/2d9a8b2c-e0ba-4143-bbfa-255dd67d4353/Backflip+Tutorial+%281%29.jpg?format=2500w",
+    },
+    {
+        title: "Introduction to C# | Step-by-Step | Comprehensive Tutorial",
+        username: "DummyUser2",
+        date: "2024-06-22T12:24:34.544Z",
+        description: "Do you have what it takes to code in C#? Try it now by reading this article. This is a comprehensive step-by-step tutorial on how to code using the C# Programming Language.",
+        coverImageUrl: "https://startupstockphotos.com/wp-content/uploads/2021/12/ssp014.jpg",
+
+    },
+]
