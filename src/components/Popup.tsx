0a4e09b9b6a0024c9aeabd85d06106b0c67c50c1
JomarMonrealUPLB <jpmonreal@up.edu.ph>
2024-06-22 19:43:54 +0800

    chore: added close button

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -2,6 +2,7 @@
 
 import { PopupContext, PopupContextType } from "@/providers/PopupProvider";
 import { ReactNode, useContext } from "react";
+import { MdClose } from "react-icons/md";
 
 export function Popup() {
 
@@ -10,7 +11,15 @@ export function Popup() {
   return <div className={`bg-[#0009] fixed inset-0 z-40 flex flex-col justify-center ${popup.isOpen? "block": "hidden"}`}>
     <div className="w-full h-full z-50" onClick={()=>popup.setIsOpen(false)}/>
     <dialog open className="z-50 w-1/2 h-4/5 p-4 overflow-auto">
+      <MdClose 
+        size={"30px"} 
+        color={"#000"} 
+        className="fixed cursor-pointer hover:scale-105" 
+        tabIndex={0}
+        onClick={()=>popup.setIsOpen(false)}/>
+      
       {popup.child}
+
     </dialog>
   </div>;
 }
