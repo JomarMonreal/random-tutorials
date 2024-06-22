a667a46f5e6501820ccbf202f26a1a6b75cbfd84
JomarMonreal <jpmonreal2035@gmail.com>
2024-06-22 22:06:09 +0800

    feat: added tags data

---
+++ src/data/tags.ts
@@ -0,0 +1,11 @@
+export const tags = [
+    { label: "RANDOM", count: 200 },
+    { label: "MATH", count: 10 },
+    { label: "SCIENCE", count: 100 },
+    { label: "PROGRAMMING", count: 20 },
+    { label: "HOME SKILL", count: 50 },
+    { label: "DANCING", count: 1 },
+    { label: "EATING", count: 7 },
+    { label: "MEDICINE", count: 8 },
+    { label: "SPORTS", count: 5 },
+]
