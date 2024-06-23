501f9a32a4c6e50970f91f1e53ce68ea98702657
JomarMonreal <jpmonreal2035@gmail.com>
2024-06-23 14:46:39 +0800

    chore: close popup in acknowledgement footer

--- src/components/AcknowledgementFooter.tsx
+++ src/components/AcknowledgementFooter.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+"use client"
+
+import React, { useContext } from 'react';
 import Link from 'next/link';
+import { PopupContext, PopupContextType } from '@/providers/PopupProvider';
 
 export function AcknowledgementFooter() {
+  const popup = useContext(PopupContext) as PopupContextType
+
   return <p className="text-center text-xs my-4">
     By continuing, you agree to Random Tutorials'
     <Link href={"/terms-of-service"}>
-      &ensp;<strong className='hover:underline'>Terms of Service</strong>&ensp;
+      &ensp;<strong className='hover:underline' onClick={()=>popup.setIsOpen(false)}>Terms of Service</strong>&ensp;
     </Link>
     and acknowledge you've read our
     <Link href={"privacy-policy"}>
-      &ensp;<strong className='hover:underline'>Privacy Policy</strong>&ensp;
+      &ensp;<strong className='hover:underline' onClick={()=>popup.setIsOpen(false)}>Privacy Policy</strong>&ensp;
     </Link>.
   </p>;
 }
