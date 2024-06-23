c3ec16dd6b10d135c20cbe33222c91e9d9542d17
JomarMonreal <jpmonreal2035@gmail.com>
2024-06-23 21:20:49 +0800

    chore: added sections

--- src/data/tutorials.ts
+++ src/data/tutorials.ts
@@ -2,27 +2,142 @@ export const tutorials = [
     {
         id: "1",
         title: "How to Open a Door?",
-        username: "DummyUser",
+        userID: "1",
         date: "2024-06-22T12:24:34.544Z",
         description: "This is a comprehensive step-by-step tutorial on how to open a door.",
         coverImageUrl: "https://i.ytimg.com/vi/xx8NZa4ptJA/maxresdefault.jpg",
-
+        tagIDs: [
+            "1",
+            "2",
+            "3"
+        ],
+        sections: [
+            {
+                header: "Step 1: Find Your Door",
+                contents: [
+                    { 
+                        type: "image", 
+                        data: {
+                            url: "https://i.ytimg.com/vi/xx8NZa4ptJA/maxresdefault.jpg", 
+                            caption: "Figure 1. Finding a door in your neighborhood"
+                        }
+                    },
+                    { 
+                        type: "paragraph",
+                        data: {
+                            text: "The first and foremost step is to find a door. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, quaerat suscipit provident repellat laborum enim eligendi nostrum repellendus voluptatibus odit quam iste labore incidunt consectetur quod magni sint at natus."
+                        }
+                    },
+                    { 
+                        type: "bulleted-list",
+                        data: {
+                            items: [
+                                "There must be a white door at your backyard.",
+                                "There must be a red door at your backyard.",
+                                "There must be a blue door at your backyard.",
+                            ]
+                        }
+                    },
+                    
+                ]
+            }
+        ]
     },
     {
         id: "2",
         title: "How to Backflip?",
-        username: "DummyUser",
+        userID: "1",
         date: "2024-06-22T12:24:34.544Z",
         description: "This is a comprehensive step-by-step tutorial on how to backflip.",
         coverImageUrl: "https://images.squarespace-cdn.com/content/v1/5eaa851bc7b0ae39a020ea32/2d9a8b2c-e0ba-4143-bbfa-255dd67d4353/Backflip+Tutorial+%281%29.jpg?format=2500w",
+        tagIDs: [
+            "4",
+            "2",
+            "3",
+            "1"
+        ],
+        sections: [
+            {
+                header: "Step 1",
+                contents: [
+                    { 
+                        type: "image", 
+                        data: {
+                            url: "https://i.ytimg.com/vi/xx8NZa4ptJA/maxresdefault.jpg", 
+                            caption: "Figure 1. Finding a door in your neighborhood"
+                        }
+                    },
+                    { 
+                        type: "paragraph",
+                        data: {
+                            text: "The first and foremost step is to find a door. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, quaerat suscipit provident repellat laborum enim eligendi nostrum repellendus voluptatibus odit quam iste labore incidunt consectetur quod magni sint at natus."
+                        }
+                    },
+                    { 
+                        type: "bulleted-list",
+                        data: {
+                            items: [
+                                "There must be a white door at your backyard.",
+                                "There must be a red door at your backyard.",
+                                "There must be a blue door at your backyard.",
+                            ]
+                        }
+                    },
+                    { 
+                        type: "paragraph",
+                        data: {
+                            text: "The first and foremost step is to find a door. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, quaerat suscipit provident repellat laborum enim eligendi nostrum repellendus voluptatibus odit quam iste labore incidunt consectetur quod magni sint at natus."
+                        }
+                    },
+                ]
+            }
+        ]
     },
     {
         id: "3",
         title: "Introduction to C# | Step-by-Step | Comprehensive Tutorial",
-        username: "DummyUser2",
+        userID: "1",
         date: "2024-06-22T12:24:34.544Z",
         description: "Do you have what it takes to code in C#? Try it now by reading this article. This is a comprehensive step-by-step tutorial on how to code using the C# Programming Language.",
         coverImageUrl: "https://startupstockphotos.com/wp-content/uploads/2021/12/ssp014.jpg",
-
+        tagIDs: [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6"
+        ],
+        sections: [
+            {
+                header: "Step 1",
+                contents: [
+                    { 
+                        type: "image", 
+                        data: {
+                            url: "https://i.ytimg.com/vi/xx8NZa4ptJA/maxresdefault.jpg", 
+                            caption: "Figure 1. Finding a door in your neighborhood"
+                        }
+                    },
+                    { 
+                        type: "paragraph",
+                        data: {
+                            text: "The first and foremost step is to find a door. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, quaerat suscipit provident repellat laborum enim eligendi nostrum repellendus voluptatibus odit quam iste labore incidunt consectetur quod magni sint at natus."
+                        }
+                    },
+                    { 
+                        type: "bulleted-list",
+                        data: {
+                            items: [
+                                "There must be a white door at your backyard.",
+                                "There must be a red door at your backyard.",
+                                "There must be a blue door at your backyard.",
+                            ]
+                        }
+                    },
+                    
+                ]
+            }
+        ]
     },
 ]
