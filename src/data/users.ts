64e581946ea0651e4d457c61cef306105abeaa40
JomarMonreal <jpmonreal2035@gmail.com>
2024-06-23 21:19:57 +0800

    feat: added dummy users

---
+++ src/data/users.ts
@@ -0,0 +1,8 @@
+export const users = [
+    {
+        id: "1",
+        username: "JMonreal",
+        email: "jpmonreal2035@gmail.com",
+        pictureUrl: "https://staudt-gmbh.com/wp-content/uploads/2018/07/person-dummy.jpg"        
+    },
+]
